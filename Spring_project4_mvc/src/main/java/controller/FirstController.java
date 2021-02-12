f1a68d21da043b43585c5b927ea9065ad7b6d3cb
roman73n87@mail.ru <roman73n87@gmail.com>
2021-02-12 09:36:22 +0300

    webcalculate via Get request

--- Spring_project4_mvc/src/main/java/controller/FirstController.java
+++ Spring_project4_mvc/src/main/java/controller/FirstController.java
@@ -1,8 +1,12 @@
 package controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,9 +25,42 @@ public class FirstController {
 	
 	@GetMapping("/goodbyePage")
 	public String byePage (@RequestParam(name = "city", required = false) String city, 
-								@RequestParam(name = "address") String address){
-		System.out.println("City: " + city + ", " + "address" + address);		
-		return "/first/bye_Page";
+								@RequestParam(name = "address") String address, Model model){
+		//System.out.println("City: " + city + ", " + "address" + address);	
+		model.addAttribute("message", "Cyti: " + city + ", " + "address " + address);
+		return "/first/bye_Page"; //возвращаем представление (jsp)
 	}
+	
+	//примитивный калькулятор
+	@GetMapping("/calculate")
+	public String  calculate (@RequestParam(name = "firstTerm") String firstTerm,
+									@RequestParam(name = "operation") String operation,
+											@RequestParam(name = "secondTerm") String secondTerm,
+																		Model model) {
+			
+		model.addAttribute("message", expres(firstTerm,secondTerm,operation));
+		
+		return "/first/calculate";
+	}
+	
+	private Integer expres(String firstTerm, String secondTerm, String oper) {
+		int i = Integer.valueOf(firstTerm);
+		int j = Integer.valueOf(secondTerm);
+		
+		switch(oper) {
+		case "+" : 
+			return i+j;
+		case "-" : 
+			return i-j;
+		case "*" : 
+			return i*j;
+		case "/" : 
+			return i/j;
+		default :
+			return 0;
+		}		
+	}
+	
+	
 	
 }
